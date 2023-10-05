4d8a633d52014d3c208ad34e5f9917fb72e0b76d
Balpreet2001 <balpreetsingh8845@gmail.com>
2023-10-05 19:59:15 +0530

    final commit: added react-speech-to-text hook along with react-copy-to-clipboard along with simple implementation of both

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import  SpeechRecognition ,{useSpeechRecognition}  from 'react-speech-recognition'
+import useClipboard from 'react-use-clipboard'
+import {useState} from 'react'
+const App = () => {
+const [textToBeCopied, setTextToBeCopied] = useState('')
+const [isCopied, setCopied] = useClipboard(textToBeCopied)
+const startListening = () => SpeechRecognition.startListening({continuous : true,language :'en-IN'})
+const {transcript, browserSupportsSpeechRecognition} = useSpeechRecognition()
+if(!browserSupportsSpeechRecognition){
+  return null
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="container">
+        <h2>Speech to Text converter</h2>
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React hook that converts speech from the microphone and make it
+          available at your react component
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <div className="main-content" onClick={() => setTextToBeCopied(transcript)}>
+          <p>
+            click here before copying
+          </p>
+          {transcript}
+        </div>
+        <div className="btn-style">
+          <button onClick={setCopied}>{isCopied? "Copied" : "Copy to Clipboard"}</button>
+          <button onClick={startListening}>Start Listening</button>
+          <button onClick={SpeechRecognition.stopListening}>Stop Listening</button>
+        </div>
+      </div>
     </div>
   );
 }
